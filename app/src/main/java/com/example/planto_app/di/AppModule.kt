671df59c35414c01b061c1b3cf06261ea9724497
Mahmoud Alkawteb <mahmoud.alkateb22@gmail.com>
2022-10-20 20:37:40 +0200

    implemented dagger hilt AppModule.kt,

---
+++ app/src/main/java/com/example/planto_app/di/AppModule.kt
@@ -0,0 +1,25 @@
+package com.example.planto_app.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.planto_app.data.entity.PlantDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun providePlantDatabase(@ApplicationContext app : Context) : PlantDatabase {
+        return Room.databaseBuilder(app,PlantDatabase::class.java, "plants")
+            .build()
+    }
+
+}
