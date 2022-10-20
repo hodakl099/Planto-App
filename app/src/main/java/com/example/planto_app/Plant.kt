2cef10488f7d543e8382d35d7b629d5cd9b380e9
Mahmoud Alkawteb <mahmoud.alkateb22@gmail.com>
2022-10-20 17:32:52 +0200

    implemented entity table name with room in Plant.kt

--- app/src/main/java/com/example/planto_app/Plant.kt
+++ app/src/main/java/com/example/planto_app/Plant.kt
@@ -1,6 +1,26 @@
 package com.example.planto_app
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "plants")
 data class Plant(
-    val name : String,
-    val plantType : String
-)
+    @PrimaryKey(autoGenerate = true)
+    var id : Int = 0,
+    @ColumnInfo(name = "plantName")
+    val plantName : String,
+    @ColumnInfo(name = "plantType")
+    val plantType : String,
+    @ColumnInfo(name = "dailyWatering")
+    val dailyWatering : Int,
+    @ColumnInfo(name = "outdoorLight")
+    val outdoorLight : Int,
+    @ColumnInfo(name = "date")
+    val date : String,
+    @ColumnInfo(name = "plantLocation")
+    val plantLocation : String,
+    @ColumnInfo(name = "plantDescription")
+    val plantDescription : String
+    )
