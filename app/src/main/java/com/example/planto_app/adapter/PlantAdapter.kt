b217f5f0c420fe12b9d895b3ce3891fe69557e9d
Mahmoud Alkawteb <mahmoud.alkateb22@gmail.com>
2022-10-20 17:44:00 +0200

    added differ call back in PlantAdapter.kt

--- app/src/main/java/com/example/planto_app/adapter/PlantAdapter.kt
+++ app/src/main/java/com/example/planto_app/adapter/PlantAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.planto_app.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.planto_app.Plant
@@ -11,9 +13,11 @@ class PlantAdapter() : RecyclerView.Adapter<PlantAdapter.PlantViewHolder>() {
 
     inner class PlantViewHolder(val binding: ItemPlantBinding) : RecyclerView.ViewHolder(binding.root)
 
+    // differ call back to calculate different between 2 lists and output it,
+    // and converts the old list to the new list, and it can be used with recyclerview adapter.
     private val differCallBack = object : DiffUtil.ItemCallback<Plant>() {
         override fun areItemsTheSame(oldItem: Plant, newItem: Plant): Boolean {
-            return oldItem.
+            return oldItem.id == newItem.id
         }
 
         override fun areContentsTheSame(oldItem: Plant, newItem: Plant): Boolean {
@@ -22,15 +26,18 @@ class PlantAdapter() : RecyclerView.Adapter<PlantAdapter.PlantViewHolder>() {
 
     }
 
+    val differ = AsyncListDiffer(this,differCallBack)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlantViewHolder {
-        TODO("Not yet implemented")
+        val binding = ItemPlantBinding.inflate(LayoutInflater.from(parent.context), parent,false)
+        return PlantViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: PlantViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val plantItem = differ.currentList[position]
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return differ.currentList.size
     }
 }
