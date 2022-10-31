36ce9c267cdbf71fd72dce60bdfd5e7fb30a55fd
Mahmoud Alkateb <mahmoud.alkateb22@gmail.com>
2022-10-31 10:28:49 +0000

    implementing open camera in AddPlantFragment.kt

--- app/src/main/java/com/example/planto_app/ui/AddPlantFragment.kt
+++ app/src/main/java/com/example/planto_app/ui/AddPlantFragment.kt
@@ -1,26 +1,38 @@
 package com.example.planto_app.ui
 
-import android.app.Activity
 import android.app.Activity.RESULT_OK
+import android.app.AlertDialog
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.provider.MediaStore
-import android.provider.MediaStore.Audio.Media
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import com.example.planto_app.MainActivity
 import com.example.planto_app.databinding.FragmentAddPlantBinding
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore
+import android.provider.Settings
+import android.widget.Toast
+import coil.load
+import coil.transform.CircleCropTransformation
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
 
 
 class AddPlantFragment : Fragment() {
 
     private var _binding : FragmentAddPlantBinding? = null
+    private val CAMERA_REQUEST_CODE = 1
+    private val GALLERY_REQUEST_CODE = 2
 
 
     private val binding get() = _binding!!
@@ -32,38 +44,154 @@ class AddPlantFragment : Fragment() {
         _binding = FragmentAddPlantBinding.inflate(layoutInflater,container,false)
 
 
-        binding.plantImg.setOnClickListener{
-//            pickImageFromGallery()
-            takePhoto()
+
+        binding.plantImg.setOnClickListener {
+            cameraCheckPermission()
         }
+
+//        binding.plantImg.setOnClickListener{
+//            val pictureDialog = AlertDialog.Builder(requireContext())
+//            pictureDialog.setTitle("Select Action")
+//            val pictureDialogItem = arrayOf("Select photo from Gallery",
+//                "Capture photo from Camera")
+//            pictureDialog.setItems(pictureDialogItem) { dialog, which ->
+//
+//                when (which) {
+//                    0 -> gallery()
+//                    1 -> camera()
+//                }
+//            }
+//
+//            pictureDialog.show()
+//        }
 
 
         return binding.root
     }
 
+    private fun galleryCheckPermission() {
 
-    //function to pick an image from the gallery.
-    private fun takePhoto() {
-//        val intent = Intent(Intent.ACTION_PICK)
-//        intent.
-//        resultLauncher.launch(intent)
-        ActivityResultContracts.TakePicture()
+        Dexter.withContext(requireContext()).withPermission(
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+        ).withListener(object : PermissionListener {
+            override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                gallery()
+            }
+
+            override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                Toast.makeText(
+                    requireContext(),
+                    "You have denied the storage permission to select image",
+                    Toast.LENGTH_SHORT
+                ).show()
+                showRotationalDialogForPermission()
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                p0: PermissionRequest?, p1: PermissionToken?) {
+                showRotationalDialogForPermission()
+            }
+        }).onSameThread().check()
+    }
+
+    private fun gallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, GALLERY_REQUEST_CODE)
     }
 
 
-//    //function to pick an image from the gallery.
-//    private fun pickImageFromGallery() {
-//        val intent = Intent(Intent.ACTION_PICK)
-//        intent.type = "image/*"
-//        resultLauncher.launch(intent)
-//    }
+    private fun cameraCheckPermission() {
+
+        Dexter.withContext(requireContext())
+            .withPermissions(
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                android.Manifest.permission.CAMERA).withListener(
+
+                object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        report?.let {
+
+                            if (report.areAllPermissionsGranted()) {
+                                camera()
+                            }
+
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        p0: MutableList<PermissionRequest>?,
+                        p1: PermissionToken?) {
+                        showRotationalDialogForPermission()
+                    }
+
+                }
+            ).onSameThread().check()
+    }
 
 
-    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == RESULT_OK) {
-            val data = result.data
-            binding.plantImg.setImageURI(data?.data)
+    private fun camera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, CAMERA_REQUEST_CODE)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+
+            when (requestCode) {
+
+                CAMERA_REQUEST_CODE -> {
+
+                    val bitmap = data?.extras?.get("data") as Bitmap
+
+                    //we are using coroutine image loader (coil)
+                    binding.plantImg.load(bitmap) {
+                        crossfade(true)
+                        crossfade(1000)
+                        transformations(CircleCropTransformation())
+                    }
+                }
+
+                GALLERY_REQUEST_CODE -> {
+
+                    binding.plantImg.load(data?.data) {
+                        crossfade(true)
+                        crossfade(1000)
+                        transformations(CircleCropTransformation())
+                    }
+
+                }
+            }
+
         }
+
+    }
+
+
+    private fun showRotationalDialogForPermission() {
+        AlertDialog.Builder(requireContext())
+            .setMessage("It looks like you have turned off permissions"
+                    + "required for this feature. It can be enable under App settings")
+
+            .setPositiveButton("Go TO SETTINGS") { _, _ ->
+
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", activity?.packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+
+            .setNegativeButton("CANCEL") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 
 
